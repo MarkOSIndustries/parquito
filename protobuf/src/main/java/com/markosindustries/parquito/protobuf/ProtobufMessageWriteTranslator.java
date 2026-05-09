b66b346d6f83271425159b7f97e518fe8828b0d1
Mark Green <markosindustries@gmail.com>
2026-05-09 20:02:03 +1000

    Fix a small bug in protobuf write translation - Empty lists were being encoded
    Other protobuf writers write a null for empty repeateds/maps, but we were writing down a field.
    This caused messages to be read which toString()ed to the same value as was written, but were
    subtly structurally different.
    
    While I was there, I memoised the decision points around what type of null checking
    is possible for each field so it's not done per value written.

--- protobuf/src/main/java/com/markosindustries/parquito/protobuf/ProtobufMessageWriteTranslator.java
+++ protobuf/src/main/java/com/markosindustries/parquito/protobuf/ProtobufMessageWriteTranslator.java
@@ -4,12 +4,15 @@ import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
 import com.markosindustries.parquito.ParquetSchemaNode;
 import com.markosindustries.parquito.WriteTranslator;
+import java.util.List;
+import java.util.function.Function;
 import org.apache.parquet.format.ConvertedType;
 
 public class ProtobufMessageWriteTranslator<Value extends Message>
     implements WriteTranslator<Value, Void> {
   private final WriteTranslator<?, ?>[] translatorsByChildIndices;
   private final Descriptors.FieldDescriptor[] fieldsDescriptorsByChildIndex;
+  private final Function<Value, Object>[] fieldGettersByChildIndex;
 
   public ProtobufMessageWriteTranslator(
       final Descriptors.Descriptor descriptor, final ParquetSchemaNode parquetSchemaNode) {
@@ -17,6 +20,7 @@ public class ProtobufMessageWriteTranslator<Value extends Message>
         new Descriptors.FieldDescriptor[parquetSchemaNode.getChildren().length];
     this.translatorsByChildIndices =
         new WriteTranslator<?, ?>[parquetSchemaNode.getChildren().length];
+    this.fieldGettersByChildIndex = new Function[parquetSchemaNode.getChildren().length];
 
     for (final var field : descriptor.getFields()) {
       final var maybeChildIndex = parquetSchemaNode.findIndexOfChildByName(field.getName());
@@ -26,42 +30,53 @@ public class ProtobufMessageWriteTranslator<Value extends Message>
             translatorsByChildIndices[childIndex] =
                 determineAppropriateTranslator(
                     field, parquetSchemaNode.getChildAtIndex(childIndex));
+            fieldGettersByChildIndex[childIndex] = determineAppropriateFieldGetter(field);
           });
     }
   }
 
   static WriteTranslator<?, ?> determineAppropriateTranslator(
       final Descriptors.FieldDescriptor field, final ParquetSchemaNode childSchemaNode) {
-    final var isMessage = field.getType() == Descriptors.FieldDescriptor.Type.MESSAGE;
     if (field.isRepeated() && childSchemaNode.getConvertedType() == ConvertedType.LIST) {
       // Repeated but not LIST would imply legacy style without the 3 layer list structure
       return new ProtobufListWriteTranslator(field, childSchemaNode);
     } else if (field.isMapField() && childSchemaNode.getConvertedType() == ConvertedType.MAP) {
       // MapField but not MAP would imply legacy style without the 3 layer map structure
       return new ProtobufMapWriteTranslator(field, childSchemaNode);
-    } else if (isMessage) {
+    } else if (field.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
       return new ProtobufMessageWriteTranslator<>(field.getMessageType(), childSchemaNode);
     } else {
       return ProtobufLeafWriteTranslator.forType(field, childSchemaNode);
     }
   }
 
-  @Override
-  public Object getField(final int childIndex, final Value value) {
-    final var fieldDescriptor = fieldsDescriptorsByChildIndex[childIndex];
-    if (!fieldDescriptor.hasPresence()) {
-      final var fieldValue = value.getField(fieldDescriptor);
-      if (fieldDescriptor.hasDefaultValue()
-          && fieldDescriptor.getDefaultValue().equals(fieldValue)) {
-        return null;
-      } else {
-        return fieldValue;
-      }
-    } else if (!value.hasField(fieldDescriptor)) {
-      return null;
+  static <Value extends Message> Function<Value, Object> determineAppropriateFieldGetter(
+      final Descriptors.FieldDescriptor field) {
+    if (field.hasPresence()) {
+      return value -> value.hasField(field) ? value.getField(field) : null;
     }
 
-    return value.getField(fieldDescriptor);
+    if (field.hasDefaultValue()) {
+      final var defaultValue = field.getDefaultValue();
+      return value -> {
+        final var fieldValue = value.getField(field);
+        return defaultValue.equals(fieldValue) ? null : fieldValue;
+      };
+    }
+
+    if (field.isRepeated()) { // maps and repeateds
+      return value -> {
+        final var fieldValue = value.getField(field);
+        return ((List<?>) fieldValue).isEmpty() ? null : fieldValue;
+      };
+    }
+
+    return value -> value.getField(field);
+  }
+
+  @Override
+  public Object getField(final int childIndex, final Value value) {
+    return fieldGettersByChildIndex[childIndex].apply(value);
   }
 
   @Override
