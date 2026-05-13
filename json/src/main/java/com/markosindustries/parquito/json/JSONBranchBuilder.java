d350002b646824c724a40b5fcf4a8d6eaeea7de0
Mark Green <markosindustries@gmail.com>
2026-05-13 21:38:09 +1000

    Bug fix - JSON reader - ByteBuffer types were written as toString of the underlying buffer, which could be massive
    Now we base64 encode the bytes the buffer represents, respecting limit and position.

--- json/src/main/java/com/markosindustries/parquito/json/JSONBranchBuilder.java
+++ json/src/main/java/com/markosindustries/parquito/json/JSONBranchBuilder.java
@@ -3,6 +3,9 @@ package com.markosindustries.parquito.json;
 import com.alibaba.fastjson2.JSONObject;
 import com.markosindustries.parquito.ParquetSchemaNode;
 import com.markosindustries.parquito.rows.BranchBuilder;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 class JSONBranchBuilder implements BranchBuilder<JSONObject> {
   private final JSONObject result;
@@ -15,7 +18,17 @@ class JSONBranchBuilder implements BranchBuilder<JSONObject> {
 
   @Override
   public void put(final int fieldIndex, final Object value) {
-    result.put(parquetSchemaNode.getChildAtIndex(fieldIndex).getElement().name, value);
+    final var schemaNode = parquetSchemaNode.getChildAtIndex(fieldIndex);
+
+    if (value instanceof ByteBuffer) {
+      result.put(
+          schemaNode.getElement().name,
+          StandardCharsets.UTF_8
+              .decode(Base64.getEncoder().encode(((ByteBuffer) value).asReadOnlyBuffer()))
+              .toString());
+    } else {
+      result.put(schemaNode.getElement().name, value);
+    }
   }
 
   @Override
